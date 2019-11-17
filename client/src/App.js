008e075d94b6ec3a56088912b773bce971262d17
Cristian Augusto <cristianaugusto2013@gmail.com>
2019-11-17 15:37:28 -0300

    Rebuild Project

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import logo from "./logo.png";
 import Launches from "./components/Launches";
 import Launch from "./components/Launch";
@@ -23,6 +23,7 @@ function App() {
           />
           <Route exact path="/" component={Launches} />
           <Route exact path="/launch/:flight_number" component={Launch} />
+          <Redirect to="/" />
         </div>
       </Router>
     </ApolloProvider>
---
+++ public/build/precache-manifest.db9cc8bcf39417c2d009fecba66a3d2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "878074d2f40beee36204b2842d8fba31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5770c17a42685364d38",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "2235088b804d97d4c1b1",
+    "url": "/static/js/2.294569d5.chunk.js"
+  },
+  {
+    "revision": "a5770c17a42685364d38",
+    "url": "/static/js/main.96e58adb.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  }
+]);
