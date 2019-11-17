cff3734c759cae947edb80c33b9fedf6c653e82c
Cristian Augusto <cristianaugusto2013@gmail.com>
2019-11-17 14:38:40 -0300

    Finished

---
+++ public/precache-manifest.606bde108519185be2ada2f0ff2c2b3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b94fac04d973e6d3e3ca07cc2731ec4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8d55e7cab583e7ba27c",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "4a57f616551ab6b27647",
+    "url": "/static/js/2.15be7cb1.chunk.js"
+  },
+  {
+    "revision": "a8d55e7cab583e7ba27c",
+    "url": "/static/js/main.03e97e7e.chunk.js"
+  },
+  {
+    "revision": "f1c6019deaec6f4a9d0b",
+    "url": "/static/js/runtime-main.11bc70d3.js"
+  }
+]);
